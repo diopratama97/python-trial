1e38a2f1b7d1888f0cb26a08c6998b8d4768d8b9
Dio Y.Pratama <dyopratama6@gmail.com>
2024-01-30 15:38:16 +0700

    first use pyhton and compile pyhton

---
+++ main.py
@@ -0,0 +1,9 @@
+print("hello world")
+print(1234)
+"""comment multiple line"""
+
+"""compile python ke bytecode dengan cara
+buka terminal jalan kan python3 -m py_compile main.py. 
+fungsi compile untuk menjalankan pyhton agar lebih cepat. karena pyhton sebelumnya itu intepreter"""
+
+"""python akan mengeksekusi dari baris per baris"""
